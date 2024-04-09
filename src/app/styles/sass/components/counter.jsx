84b321e7987e1eb3ae79e8fc2a68db97a5ee94db
uxeeCoder <umar.afar@aol.com>
2024-04-08 21:24:59 -0400

    removed unused code

--- src/app/styles/sass/components/counter.jsx
+++ src/app/styles/sass/components/counter.jsx
@@ -1,9 +1,9 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 
-import React from 'react'
+// import React from 'react'
 
-export default function counter() {
-    const [count, setCount] = useState(0);
+// export default function counter() {
+//     const [count, setCount] = useState(0);
     
 //   return (
 //     <div onClick={()=> setCount (prviousCount)=> previousCount +1}}>counter</div>

5007cc9fcc76bc418e25b73e5e8b05153fca5fbc
uxeeCoder <umar.afar@aol.com>
2024-04-08 20:59:45 -0400

    Just adding files quickly to connect with netlify

---
+++ src/app/styles/sass/components/counter.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+import React from 'react'
+
+export default function counter() {
+    const [count, setCount] = useState(0);
+    
+//   return (
+//     <div onClick={()=> setCount (prviousCount)=> previousCount +1}}>counter</div>
+//   )
+// }
